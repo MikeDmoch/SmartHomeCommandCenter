09984ab90a4a7947abc5d550a8874d09a8687dbd
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-01-18 10:30:08 +0100

    Add project files.

---
+++ SmartHomeCommandCenter/Light.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeCommandCenter
+{
+    internal class Light
+    {
+        public bool lightTurnedOn = false;
+
+        public void Switch()
+        {
+            lightTurnedOn = !lightTurnedOn;
+            if (lightTurnedOn)
+            {
+                Console.WriteLine("Light turned off");
+            }
+            else Console.WriteLine("Light turned on");
+        }
+    }
+}
---
+++ SmartHomeCommandCenter/Thermostat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeCommandCenter
+{
+    internal class Thermostat
+    {
+        public void SetTemperature(int temperature) => Console.WriteLine($"Temperature set to {temperature}");
+    }
+}
