29230c620403791130f6683e27f85468110d9b6a
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-01-18 10:54:45 +0100

    added SmartHomeManager

--- SmartHomeCommandCenter/PlayMusic.cs
+++ SmartHomeCommandCenter/PlayMusic.cs
@@ -8,5 +8,17 @@ namespace SmartHomeCommandCenter
 {
     internal class PlayMusic : ICommand
     {
+        private readonly AudioSystem audioSystem;
+        private readonly string genre;
+
+        public PlayMusic(AudioSystem audioSystem, string genre)
+        {
+            this.audioSystem = audioSystem;
+            this.genre = genre;
+        }
+    
+        public void Execute() => audioSystem.SetPlaylist(genre);
+
+        public void Undo() => audioSystem.StopMusic();
     }
 }
---
+++ SmartHomeCommandCenter/SmartHomeManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeCommandCenter
+{
+    internal class SmartHomeManager
+    {
+        public readonly Stack<ICommand> history = new();
+
+        public void ExecuteCommant(ICommand command)
+        {
+            command.Execute();
+            history.Push(command);
+        }
+        public void Undo()
+        {
+            if (history.Count > 0)
+            {
+                var tmp = history.Pop();
+                tmp.Undo();
+            }
+            else Console.WriteLine("Nothing to undo");
+        }
+    }
+}
